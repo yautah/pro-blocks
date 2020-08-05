f93110727aadcd779cab77b883ec6dfd03009547
yangquan <yangquan@mchina.cn>
2020-08-05 15:42:50 +0800

    no message

--- TableListModal/src/config.js
+++ TableListModal/src/config.js
@@ -1,3 +1,10 @@
+const routes =  {
+  name: 'BLOCK_NAME_CAMEL_CASE',
+  icon: 'team',
+  path: 'ROUTE_PATH',
+  component: 'ROUTE_PATH',
+}
+
 const locals = {
   'menu.BLOCK_NAME_CAMEL_CASE': '记录列表',
 }
